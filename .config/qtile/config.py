bfc19d22807379b384b4b75fff4e9045367fec9d
user <user@vostro.attlocal.net>
2021-04-27 10:18:46 -0700

    push up personal config, need to change to adapt to enos

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -40,26 +40,40 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
+    Key([mod],
+        "space",
+        lazy.layout.next(),
         desc="Move window focus to other window"),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+    Key([mod, "shift"],
+        "h",
+        lazy.layout.shuffle_left(),
         desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+    Key([mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
         desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+    Key([mod, "shift"],
+        "j",
+        lazy.layout.shuffle_down(),
         desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
+    Key([mod, "control"],
+        "h",
+        lazy.layout.grow_left(),
         desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
+    Key([mod, "control"],
+        "l",
+        lazy.layout.grow_right(),
         desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
+    Key([mod, "control"],
+        "j",
+        lazy.layout.grow_down(),
         desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -68,17 +82,23 @@ keys = [
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+    Key([mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
+    Key([mod, "shift", "control"], "h", lazy.layout.swap_column_left()),
+    Key([mod, "shift", "control"], "l", lazy.layout.swap_column_right()),
+    Key([mod, "shift"], "space", lazy.layout.flip()),
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
+    Key([mod],
+        "r",
+        lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
 ]
 
@@ -87,11 +107,15 @@ groups = [Group(i) for i in "123456789"]
 for i in groups:
     keys.extend([
         # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
+        Key([mod],
+            i.name,
+            lazy.group[i.name].toscreen(),
             desc="Switch to group {}".format(i.name)),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+        Key([mod, "shift"],
+            i.name,
+            lazy.window.togroup(i.name, switch_group=True),
             desc="Switch to & move focused window to group {}".format(i.name)),
         # Or, use below if you prefer not to switch to that group.
         # # mod1 + shift + letter of group = move focused window to group
@@ -100,8 +124,9 @@ for i in groups:
     ])
 
 layouts = [
-    
-    layout.Bsp(border_focus = "#8b57de", border_normal = "#5d4485", margin = 4),
+    layout.MonadTall(margin=4, border_focus='#a45ee0',
+                     border_normal='#531f80'),
+    #layout.Columns(border_focus_stack='#d75f5f'),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -117,8 +142,8 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font='Jetbrains Mono',
-    fontsize=14,
+    font='CaskaydiaCove Nerd Font Mono',
+    fontsize=16,
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
@@ -127,8 +152,24 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
+                widget.CurrentLayoutIcon(),
+                widget.TextBox(font="CaskaydiaCove Nerd Font Mono",
+                               foreground="#331f66",
+                               text="◢",
+                               fontsize=54,
+                               padding=-5),
+                widget.GroupBox(highlight_method='line',
+                                this_screen_border="#a45ee0",
+                                this_current_screen_border="#a45ee0",
+                                active="#ffffff",
+                                inactive="#86768a",
+                                background="#331f66"),
+                widget.TextBox(
+                    font="CaskaydiaCove Nerd Font Mono",
+                    foreground="#331f66",
+                    text="◤ ",
+                    fontsize=44,
+                    padding=-10),
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.Chord(
@@ -137,22 +178,36 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
+                widget.TextBox(font="CaskaydiaCove Nerd Font Mono",
+                               foreground="#331f66",
+                               text="◢",
+                               fontsize=54,
+                               padding=-5),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p',
+                             background="#331f66"),
+                widget.TextBox(
+                    font="CaskaydiaCove Nerd Font Mono",
+                    foreground="#331f66",
+
+                    text="◤ ",
+                    fontsize=44,
+                    padding=-10),
+                widget.QuickExit()
             ],
             24,
-        ),
-    ),
+        ), ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
+    Drag([mod],
+         "Button1",
+         lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
+    Drag([mod],
+         "Button3",
+         lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
