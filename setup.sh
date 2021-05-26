74619b33b45fccd3eb308968ff8b617238f72773
codic12 <codic@linuxmail.org>
2021-05-25 17:51:38 -0700

    fix stuff up. please do test

--- setup.sh
+++ setup.sh
@@ -7,13 +7,15 @@ if [ "$x" = "yes" ]
 then
   echo "Presuming that all dependencies already installed..."
 
-  rm -f ~/.Xresources ~/.gtkrc-2.0
+  rm -f ~/.Xresources ~/.gtkrc-2.0 ~/.config/picom.conf
   cp .Xresources ~/
   cp .gtkrc-2.0 ~/
-  rm -rf ~/.config/gtk-3.0
+  cp .config/picom.conf ~/.config/picom.conf
+  rm -rf ~/.config/gtk-3.0/
   cp -r .config/gtk-3.0 ~/.config/
-  rm -rf ~/.config/qtile
+  rm -rf ~/.config/qtile/
   cp -r .config/qtile ~/.config/
-  
+  rm -rf ~/.config/rofi/
+  cp -r .config/rofi ~/.config/
   echo 'done'
 fi
