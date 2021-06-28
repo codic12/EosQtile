fccc57188b1554e887556af0d7cc07b8b87a629a
codic12 <codic@linuxmail.org>
2021-06-28 10:16:45 -0700

    fix typo in autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,2 +1,2 @@
-feh --bg-scale /usr/share/endeavouros/backgrounds/ endeavouros-wallpaper.png
+feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
 picom --experimental-backends --vsync & disown # should prevent screen tearing on most setups
